ad7453109a711785d5b6cfbce211626c3ace1b65
Aleksandr Tsygankov <tsygankov.aleksandr@gmail.com>
2017-08-29 01:13:02 +0700

    fix missing getData selector

--- reducers/selectors.js
+++ reducers/selectors.js
@@ -1,2 +1,3 @@
+export const getData = state => state.main.data;
 export const getAddNewModalShown = state => state.main.add_new_modal_shown;
 export const getRecordingPermissions = state => state.main.recording_permissions;
