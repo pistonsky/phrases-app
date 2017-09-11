18ffe1e2a5051d16a28de693629fdd4197dd8ae7
Aleksandr Tsygankov <tsygankov.aleksandr@gmail.com>
2017-09-11 12:56:21 +0800

    production API base url

--- utils/config.js
+++ utils/config.js
@@ -1,3 +1,2 @@
-// export const BASE_URL = 'https://pistonsky-phrases.herokuapp.com';
-export const BASE_URL = 'http://localhost:5000';
+export const BASE_URL = 'https://pistonsky-phrases.herokuapp.com';
 export const UPLOAD_URL = BASE_URL + '/upload';
