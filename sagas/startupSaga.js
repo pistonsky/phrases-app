174b67f90f7bfdcf1b7669eaad095dca879f9500
Aleksandr Tsygankov <tsygankov.aleksandr@gmail.com>
2017-09-18 20:04:20 +0800

    bugfix: correct way to check if router is ready

--- sagas/startupSaga.js
+++ sagas/startupSaga.js
@@ -1,12 +1,14 @@
-import { take, select, call } from 'redux-saga/effects';
+import { all, take, select, call } from 'redux-saga/effects';
 import { REHYDRATE } from 'redux-persist/constants';
 import { getUserId } from '../reducers/selectors';
 import { Actions } from 'react-native-router-flux';
 import { ROUTER_READY } from '../actions/types';
 
 const startupSaga = function* startupSaga() {
-  yield take(ROUTER_READY);
-  yield take(REHYDRATE);
+  yield all([
+    take(ROUTER_READY),
+    take(REHYDRATE)
+  ]);
   const user_id = yield select(getUserId);
   if (user_id) {
     yield call(Actions.main);
