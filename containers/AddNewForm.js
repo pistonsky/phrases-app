1db2ebbf868b43349c37741149ee4bd8e27b1622
Aleksandr Tsygankov <tsygankov.aleksandr@gmail.com>
2017-09-27 19:50:38 +0800

    do not use KeyboardAvoidingView since keyboard only hides button

--- containers/AddNewForm.js
+++ containers/AddNewForm.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {
-  KeyboardAvoidingView,
   View,
+  View as KeyboardAvoidingView,
   Text,
   Dimensions,
   Image,
