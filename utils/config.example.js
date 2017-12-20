1badc2affc846e0aac376e3164c8c6360ed0b853
Aleksandr Tsygankov <tsygankov.aleksandr@gmail.com>
2017-12-20 13:58:33 +0800

    don't store config in git

--- utils/config.example.js
+++ utils/config.example.js
@@ -3,5 +3,5 @@ export const UPLOAD_URL = BASE_URL + '/upload';
 export const BASE_AUDIO_URL = 'https://pistonsky-phrases.s3.amazonaws.com/';
 export const S3_BUCKET = 'pistonsky-phrases';
 export const S3_REGION = 'ap-southeast-1';
-export const S3_ACCESS_KEY = 'AKIAJJ7F7NZE53HD66SA';
-export const S3_SECRET_KEY = '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';
+export const S3_ACCESS_KEY = '';
+export const S3_SECRET_KEY = '';
